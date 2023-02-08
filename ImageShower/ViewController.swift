94c35e6f4531b6f74aa7a381c46835ab14907b9f
Devnir <to.devnir@gmail.com>
2023-02-08 12:53:10 +0200

    Initial Commit

---
+++ ImageShower/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ImageShower
+//
+//  Created by Андрей Белогородский on 08.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
